2f3d9ad328d5a2ca73cc949cb88a9bb715faadfd
JheysonDeotti <jheyson007@hotmail.com>
2024-06-10 22:57:24 -0300

    adicionado operadores de comparação

--- arquivo.js
+++ arquivo.js
@@ -1,14 +1,16 @@
 
-function generateNumber(){
+function generateNumber() {
 
     const min = Math.ceil(document.querySelector(".input-start").value)
     const max = Math.floor(document.querySelector(".input-final").value)
 
-    const result = Math.floor(Math.random() * (max - min + 1)) + min;
+    if (min >= max) {
+        alert("O valor minimo tem que ser MAIOR que o valor máximo!")
+    } else {
+        const result = Math.floor(Math.random() * (max - min + 1)) + min;
 
-alert(result)
+        alert(result)
 
-
-
+    }
 }
 
